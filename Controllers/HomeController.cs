ba80101052a1ded5ffc3000e411086f2026704d3
Hema Madhavan <madavan174@yahoo.in>
2021-12-02 23:42:20 +0530

    Initializing repository

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using videowebapp.Models;
+using Microsoft.ApplicationInsights;
+
+namespace videowebapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private TelemetryClient _aiClient;
+
+        public HomeController(ILogger<HomeController> logger, TelemetryClient aiClient)
+        {
+            _logger = logger;
+            _aiClient = aiClient;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public ActionResult Like(string button)
+        {
+            ViewBag.Message = "Thank you for your response";
+            _aiClient.TrackEvent("LikeClicked");
+            return View("Index");
+        }
+    }
+}
